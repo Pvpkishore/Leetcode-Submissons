57970b1964cc9c96df2811c03505a1c6f2d74a3c
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-08-03 23:35:33 +0530

    Time: 16 ms (68.12%), Space: 28 MB (8.99%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for(auto it:nums){
+            map[it]++;
+        }
+        for(auto it:nums){
+              if(map[it]>(nums.size()/2)){
+                return it;
+              }
+        }
+        return 0; 
+}
+};
