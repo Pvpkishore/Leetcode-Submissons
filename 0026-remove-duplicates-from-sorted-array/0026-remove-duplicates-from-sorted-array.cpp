0602651721d73ac99ca084c2d1efd20a00cfd0ef
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-08-03 23:26:12 +0530

    Time: 8 ms (54.17%), Space: 21.3 MB (31.52%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        if(nums.size()==0){
+            return 0;
+        }
+        else{
+            int j=0;
+          for(int i=1;i<nums.size();i++){
+            if(nums[j]!=nums[i]){
+                nums[++j]=nums[i];
+            }
+          }
+          return j+1;
+        }
+
+        }
+};
