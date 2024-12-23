e1bedec4731b90a2914db3bdfa0bde7d10811bb9
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-12-23 15:18:10 +0530

    Time: 0 ms (100%), Space: 12 MB (22.13%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans;
-        int i=0;
-        while(true){
-            char curr_ch=0;
-            for(auto str:strs){
-                if(i>=str.size()){
-                    curr_ch=0;
-                    break;
-                }
-                if(curr_ch==0){
-                    curr_ch=str[i];
-                }
-                else if(str[i]!=curr_ch){
-                    curr_ch=0;
-                    break;
-                }
-          }
-            if(curr_ch==0){
-                break;
+    string longestCommonPrefix(vector<string>& v) {
+        string ans="";
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
             }
-            ans.push_back(curr_ch);
-            i++;
+            ans+=first[i];
         }
         return ans;
     }
