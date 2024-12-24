b59e57a161d5ac389450dd4e2d4faee76603f761
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-12-24 19:27:16 +0530

    Time: 2 ms (82.32%), Space: 13.3 MB (37.17%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanMap = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int result = 0;
+      for(int i=0;i < s.size();i++){
+         if (i < s.size() - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+                // Apply subtraction rule
+                result -= romanMap[s[i]];
+            } else {
+                // Add value normally
+                result += romanMap[s[i]];
+            }
+      }
+       return result;
+    }
+};
