c202751f1313db16ee9275b14033c516bcfffbbd
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-01-06 21:56:34 +0530

    Time: 0 ms (100%), Space: 21.8 MB (28.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         int i;
-    sort(nums.begin(),nums.end());
-        for( i=0;i<nums.size();i++){
-            if(i!=nums[i]){
-                break;
-            }
-        }
-        return i;
-     }
+    //      int i;
+    // sort(nums.begin(),nums.end());
+    //     for( i=0;i<nums.size();i++){
+    //         if(i!=nums[i]){
+    //             break;
+    //         }
+    //     }
+    //     return i;
+    //  }
+    int ans=nums.size();
+    for(int i=0;i<nums.size();i++){
+        ans=ans^nums[i];
+        ans=ans^i;
+        
+    }
+
+    return ans;
+    }
 };
