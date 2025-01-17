0a86270b8f34b1c8e3907717b51ca56a8d72c6f6
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-01-17 12:17:37 +0530

    Time: 6 ms (90.78%), Space: 11.8 MB (48.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,22 +10,43 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
 
-        unordered_map<ListNode*,bool> m;
+//APPROACH-1
+    //     unordered_map<ListNode*,bool> m;
 
-        ListNode* temp =head;
+    //     ListNode* temp =head;
 
-    while(temp!=NULL){
+    // while(temp!=NULL){
 
-        if(m[temp]==true){
-            //Already one time trversed so it exists once
+    //     if(m[temp]==true){
+    //         //Already one time trversed so it exists once
+    //         return true;
+    //     }
+    //     else{
+    //         //otherwise mark it as true
+    //         m[temp]=true;
+    //     }
+    //     temp=temp->next;
+    // }
+    //     return false;
+    // }
+//APPROACH-2
+
+  ListNode* slow =head;
+  ListNode* fast =head;
+
+  while(fast!=NULL){
+
+    fast =fast->next;
+
+           if(fast!=NULL){
+             slow=slow->next;
+             fast=fast->next;
+           }
+
+           if(fast==slow){
             return true;
-        }
-        else{
-            //otherwise mark it as true
-            m[temp]=true;
-        }
-        temp=temp->next;
-    }
-        return false;
+           }
+  }
+  return false;
     }
 };
