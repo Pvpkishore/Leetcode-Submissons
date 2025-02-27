c3e41694fe5e25bc48d0e26b2933120e3cb80f0c
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-02-27 23:23:43 +0530

    Time: 147 ms (5.07%), Space: 14.1 MB (60.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-     vector<int> res;
-for(int i=0;i<nums.size();i++){
-int j=i+1;
-while(j<nums.size()){
-if(nums[i]+nums[j]==target){
-res.push_back(i);
-res.push_back(j);
-}
-j++;
-}
-}
-return res;
+        vector<int> res;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            while (j < nums.size()) {
+            if (nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                }
+                j=j+1;
+            }
+        }
+        return res;
     }
 };
