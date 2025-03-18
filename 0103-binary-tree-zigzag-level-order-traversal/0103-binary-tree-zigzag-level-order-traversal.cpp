ed62c046e479f2f9e8467e282d70e540d35eff53
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-03-19 00:12:49 +0530

    Time: 1 ms (17.39%), Space: 15.4 MB (6.82%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+          vector<vector<int>> result; // Stores level-wise nodes
+
+        if (root == NULL)
+            return result; // If tree is empty, return empty vector
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true; // Track order of insertion
+
+        while (!q.empty()) {
+            int levelSize = q.size(); // Number of nodes at the current level
+            deque<int> currentLevel; // Stores values at the current level
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* front = q.front();
+                q.pop();
+
+                // Insert values based on direction
+                if (leftToRight)
+                    currentLevel.push_back(front->val);
+                else
+                    currentLevel.push_front(front->val);
+
+                // Push child nodes to queue
+                if (front->left != NULL)
+                    q.push(front->left);
+                if (front->right != NULL)
+                    q.push(front->right);
+            }
+
+            result.push_back({currentLevel.begin(), currentLevel.end()}); // Store current level
+            leftToRight = !leftToRight; // Toggle direction for next level
+        }
+
+        return result;
+    }
+};
