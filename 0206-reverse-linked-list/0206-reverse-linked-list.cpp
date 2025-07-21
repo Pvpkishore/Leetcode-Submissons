e0c8cf2b3fc87517689424cb650d2ab570f86ac0
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-21 10:27:31 +0530

    Time: 0 ms (100%), Space: 13.3 MB (69.92%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -32,21 +32,18 @@ public:
 //    }
     ListNode* reverseList(ListNode* head) {
 
-        // ListNode* prev = NULL; 
-        // ListNode* curr = head;
-        // ListNode* newHead = reverseList(prev,curr);
-        // return newHead;
-
         //iterative Approach
 
-        ListNode* prev =NULL;
-        ListNode* curr =head;
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
 
         while(curr!=NULL){
-             
+            
              ListNode* forward = curr->next;
-
              curr->next = prev;
+             
              prev = curr;
              curr = forward;
 
