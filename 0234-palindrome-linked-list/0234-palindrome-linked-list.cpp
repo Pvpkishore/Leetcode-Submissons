b888cd697cb4e0faff71a9c7bae68226577d2f9a
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-24 22:41:14 +0530

    Time: 0 ms (100%), Space: 126.4 MB (33.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,38 +13,52 @@ public:
  bool isPalindrome(ListNode* head) {  
 
         if (!head || !head->next) return true;
-        //1.Find the middle Node
-        ListNode* slow = head;
-        ListNode* fast = head;
+    //     //1.Find the middle Node
+    //     ListNode* slow = head;
+    //     ListNode* fast = head;
 
-        while(fast!=NULL){
-            fast=fast->next;
-            if(fast!=NULL){
-                slow=slow->next;
-                fast=fast->next;
-            }
-        }
+    //     while(fast!=NULL){
+    //         fast=fast->next;
+    //         if(fast!=NULL){
+    //             slow=slow->next;
+    //             fast=fast->next;
+    //         }
+    //     }
 
-        //2. Reverse Second half
-        ListNode* prev = nullptr;
-        while(slow){
-            ListNode* forward = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = forward;
-        }
+    //     //2. Reverse Second half
+    //     ListNode* prev = nullptr;
+    //     while(slow){
+    //         ListNode* forward = slow->next;
+    //         slow->next = prev;
+    //         prev = slow;
+    //         slow = forward;
+    //     }
 
-        //3.Compare Both Halves
-        ListNode* left = head;
-        ListNode* right = prev;
-        while(right){
-            if(left->val!=right->val){
-                return false;
-            }
-            left = left->next;
-            right = right->next;
-        }
+    //     //3.Compare Both Halves
+    //     ListNode* left = head;
+    //     ListNode* right = prev;
+    //     while(right){
+    //         if(left->val!=right->val){
+    //             return false;
+    //         }
+    //         left = left->next;
+    //         right = right->next;
+    //     }
 
-    return true;
+    // return true;
+
+    ListNode* dummy = head;
+    string s ="";
+    string Sreversed;
+
+    while(dummy!=NULL){
+        s+=dummy->val;
+        dummy = dummy->next;
+    }
+
+    Sreversed = s;
+    reverse(Sreversed.begin(),Sreversed.end());
+
+      return s==Sreversed;
     }
 };
