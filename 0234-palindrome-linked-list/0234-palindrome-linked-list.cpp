bef6fed9cffbf4397da20dafa12e17590b063f7a
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-24 22:36:05 +0530

    Time: 4 ms (53.41%), Space: 118.1 MB (69.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,84 +10,41 @@
  */
 class Solution {
 public:
+ bool isPalindrome(ListNode* head) {  
 
+        if (!head || !head->next) return true;
+        //1.Find the middle Node
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-    ListNode* GetMiddle(ListNode* head,ListNode* &Middlenodekaprev){
-      ListNode* slow= head;
-      ListNode* fast =head;
-
-      while(fast!=NULL){
-        fast=fast->next;
-
-        if(fast!=NULL){
+        while(fast!=NULL){
             fast=fast->next;
-            Middlenodekaprev = slow;
-            slow=slow->next;
-        }
-      }
-      return slow;
-
-    }
-    ListNode* ReverseList(ListNode* &prev ,ListNode* &curr){
-        while(curr!=NULL){
-
-            ListNode *forward = curr->next;
-            curr->next =prev;
-            prev=curr;
-            curr=forward;
-        }
-        return prev;
-     }
-    
-    bool isPalindrome(ListNode* head) {
-
-        if(head==NULL){
-            //LL is empty
-            return true;
+            if(fast!=NULL){
+                slow=slow->next;
+                fast=fast->next;
+            }
         }
 
-        if(head->next ==NULL){
-            //LL is single
-            return true;
+        //2. Reverse Second half
+        ListNode* prev = nullptr;
+        while(slow){
+            ListNode* forward = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = forward;
         }
-        
-        ListNode* firsthalfhead = head;
-        //travel till the middle node and break into two halves
-        ListNode* Middlenodekaprev =NULL;
 
-        ListNode* Middlenode = GetMiddle(head ,Middlenodekaprev);
-         //break
-        Middlenodekaprev -> next = NULL;
-
-        //reverse the second half
-        ListNode* prev =NULL;
-        ListNode* curr = Middlenode;
-        ListNode* secondhalfHead = ReverseList(prev,curr);
-
-        //compare both halfs to decide true or false
-
-        ListNode* temphead1 = firsthalfhead;
-        ListNode* temphead2 = secondhalfHead;
-
-        while(temphead1!=NULL){
-            if(temphead1->val != temphead2->val ){
-
+        //3.Compare Both Halves
+        ListNode* left = head;
+        ListNode* right = prev;
+        while(right){
+            if(left->val!=right->val){
                 return false;
             }
-            else{
-                temphead1 =temphead1->next;
-                temphead2 =temphead2->next;
-
-            }
+            left = left->next;
+            right = right->next;
         }
 
-        return true;
-
-
-
-
-
-        
-        
+    return true;
     }
 };
