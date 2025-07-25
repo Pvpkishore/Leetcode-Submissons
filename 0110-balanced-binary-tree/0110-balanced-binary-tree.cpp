9d0b4a1b3ede9fe2e71809c4505240320b210e18
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-25 19:59:18 +0530

    Time: 81 ms (5.13%), Space: 23.2 MB (5.66%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -16,28 +16,36 @@ public:
         if(root==NULL){
             return 0;
         }
+
         int leftsubtreeheight =  Getheight(root->left);
         int rightsubtreeheight =  Getheight(root->right);
+
         int maxheight = max(leftsubtreeheight,rightsubtreeheight);
         int totalheight = maxheight + 1;
+
         return totalheight;
 
     }
 
     bool isBalanced(TreeNode* root) {
-        if(root ==NULL){
+
+        if(root == NULL){
             return true;
         }
+        
         //checking in current Node
         //one case we have to solve remaining will be done by the recursion
         int leftheight = Getheight(root->left);
         int Rightheight = Getheight(root->right);
+
         int absdiff = abs(leftheight - Rightheight);
+
         bool status = (absdiff<=1);
 
         //Remaining recursion will do
          bool leftans = isBalanced(root->left);
          bool rightans = isBalanced(root->right);
+
 
          //I can say whether tree is balanced or not,when
          //current Node is balanced
