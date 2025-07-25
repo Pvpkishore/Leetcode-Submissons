5f349327198cafee9810998df46ab23f1df435ba
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-25 20:04:14 +0530

    Time: 272 ms (11.02%), Space: 26 MB (5.15%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -13,13 +13,17 @@ class Solution {
 public:
 
     int getheight(TreeNode* root){
+
         if(root ==NULL){
             return 0;
         }
+
         int leftsubtreeheight = getheight(root->left);
         int rightsubtreeheight = getheight(root->right);
+
         int maximumheight = max(leftsubtreeheight,rightsubtreeheight);
         int totalheight = maximumheight+1;
+
         return totalheight;
     }
 
@@ -28,11 +32,14 @@ public:
       if(root==NULL){
         return 0;
       }
+
       int option1 =  diameterOfBinaryTree(root->left);
       int option2 =  diameterOfBinaryTree(root->right);
       int option3 =  getheight(root->left) + getheight(root->right);
 
-      int maxdiameter = max(option1,max(option2,option3));
+      int maxdiameter = max({option1,option2,option3});
+      
       return maxdiameter;
+
     } 
 };
