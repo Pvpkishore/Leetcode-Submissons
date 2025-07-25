d7853067d43b90fa8a96c5e5dd3a41599f5fb758
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-25 20:08:59 +0530

    Time: 3 ms (25.45%), Space: 23.7 MB (72.36%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,45 +1,67 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+
+//     int getheight(TreeNode* root){
+
+//         if(root ==NULL){
+//             return 0;
+//         }
+
+//         int leftsubtreeheight = getheight(root->left);
+//         int rightsubtreeheight = getheight(root->right);
+
+//         int maximumheight = max(leftsubtreeheight,rightsubtreeheight);
+//         int totalheight = maximumheight+1;
+
+//         return totalheight;
+//     }
+
+//     int diameterOfBinaryTree(TreeNode* root) {
+        
+//       if(root==NULL){
+//         return 0;
+//       }
+
+//       int option1 =  diameterOfBinaryTree(root->left);
+//       int option2 =  diameterOfBinaryTree(root->right);
+//       int option3 =  getheight(root->left) + getheight(root->right);
+
+//       int maxdiameter = max({option1,option2,option3});
+      
+//       return maxdiameter;
+
+//     } 
+// };
 class Solution {
 public:
 
-    int getheight(TreeNode* root){
+    int maxDiameter =0;
 
-        if(root ==NULL){
-            return 0;
-        }
+    int height(TreeNode* root) {
+        
+        if(root==NULL) return 0;
 
-        int leftsubtreeheight = getheight(root->left);
-        int rightsubtreeheight = getheight(root->right);
+        int left =  height(root->left);
+        int right = height(root->right);
 
-        int maximumheight = max(leftsubtreeheight,rightsubtreeheight);
-        int totalheight = maximumheight+1;
+        maxDiameter = max(maxDiameter , left + right);
 
-        return totalheight;
+        return 1 + max(left , right);
     }
 
-    int diameterOfBinaryTree(TreeNode* root) {
-        
-      if(root==NULL){
-        return 0;
-      }
-
-      int option1 =  diameterOfBinaryTree(root->left);
-      int option2 =  diameterOfBinaryTree(root->right);
-      int option3 =  getheight(root->left) + getheight(root->right);
-
-      int maxdiameter = max({option1,option2,option3});
-      
-      return maxdiameter;
-
-    } 
+     int diameterOfBinaryTree(TreeNode* root) {
+        height(root); // starts postorder traversal
+        return maxDiameter;
+    }
 };
