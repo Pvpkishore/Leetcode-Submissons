1c0e258bb95be75e52c0a2abe1a364ce9941cb3c
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-26 11:13:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,20 +2,62 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
          
-        int n = nums.size();
-        unordered_map <int,int> mp;
-        vector<int> ans;
-        for(auto num:nums){
-           mp[num]++;
+        // int n = nums.size();
+        // unordered_map <int,int> mp;
+        // vector<int> ans;
+        // for(auto num:nums){
+        //    mp[num]++;
+        // }
+
+
+        // for(auto num:mp){
+        //     if(num.second>n/3){
+        //        ans.push_back(num.first);
+        //     }
+        // }
+
+        // return ans;
+ // Boyer-Moore Majority Vote Algorithm  space complexity is O(1)
+      int count1 = 0,count2=0;
+      int candidate1 = 0 ,candidate2=0;
+
+    for(auto num:nums){
+       if(num==candidate1){
+           count1++;
         }
-
-
-        for(auto num:mp){
-            if(num.second>n/3){
-               ans.push_back(num.first);
-            }
+        else if(num==candidate2){
+            count2++;
         }
+        else if(count1==0){
+            candidate1 = num;
+            count1=1;
+        }
+        else if(count2==0){
+        candidate2 = num;
+        count2=1;
+        }
+        else{
+        count1--;
+        count2--;
+       }
+    }
+    //second pass for verfication
 
-        return ans;
+    count1=0;
+    count2=0;
+
+    for(auto num:nums){
+        if(num==candidate1) count1++;
+        if(num==candidate2) count2++;
+    }
+
+    vector<int> result;
+    int n=nums.size();
+
+    if(count1>n/3) result.push_back(candidate1);
+    if(count2>n/3) result.push_back(candidate2);
+
+    return result;
+
     }
 };
