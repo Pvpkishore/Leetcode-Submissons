1d5468d7a56e4916b4d6279c02a60e857194815f
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-07-30 23:18:34 +0530

    Time: 0 ms (100%), Space: 11.2 MB (30.67%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int left = 0;
-        int right = s.length() - 1;
+        // int left = 0;
+        // int right = s.length() - 1;
 
-        // Trim leading spaces
-        while (left <= right && s[left] == ' ') left++;
+        // // Trim leading spaces
+        // while (left <= right && s[left] == ' ') left++;
 
-        // Trim trailing spaces
-        while (right >= left && s[right] == ' ') right--;
+        // // Trim trailing spaces
+        // while (right >= left && s[right] == ' ') right--;
 
-        string temp = "";
-        string ans = "";
+        // string temp = "";
+        // string ans = "";
 
-        while (left <= right) {
-            char ch = s[left];
-            if (ch != ' ') {
-                temp += ch;
-            } else if (temp != "") { // Only add the word if it's not empty
-                if (ans != "") {
-                    ans = temp + " " + ans;
-                } else {
-                    ans = temp;
-                }
-                temp = "";
+        // while (left <= right) {
+        //     char ch = s[left];
+        //     if (ch != ' ') {
+        //         temp += ch;
+        //     } else if (temp != "") { // Only add the word if it's not empty
+        //         if (ans != "") {
+        //             ans = temp + " " + ans;
+        //         } else {
+        //             ans = temp;
+        //         }
+        //         temp = "";
+        //     }
+        //     left++;
+        // }
+
+        // // Add the last word if it exists
+        // if (temp != "") {
+        //     if (ans != "") ans = temp + " " + ans;
+        //     else ans = temp;
+        // }
+
+        // return ans;
+
+        vector<string> words;
+        string word;
+
+        stringstream ss(s);
+
+        while(ss>>word){
+            words.push_back(word);
+        }
+       
+        reverse(words.begin(),words.end());
+
+        s.clear();
+        
+        for(int i=0;i<words.size();i++){
+            s+=words[i];
+            if(i<words.size()-1){
+                s+=" ";
             }
-            left++;
         }
-
-        // Add the last word if it exists
-        if (temp != "") {
-            if (ans != "") ans = temp + " " + ans;
-            else ans = temp;
-        }
-
-        return ans;
+       return s;
     }
 };
