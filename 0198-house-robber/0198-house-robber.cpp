7aa12ccef4a409cab5653dca37929fc1a802dac5
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-08-02 00:45:21 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -22,24 +22,44 @@ public:
        }
 
       //step:-3 check if it is already it is existed in dp or not
-
        if(dp[i]!=-1){
         return dp[i];
        }
-
        //step2:-If ans coming with recursion store ans in dp and return
        int include = nums[i] + SolveusingMem(dp,nums,i+2);
        int exclude = 0 + SolveusingMem(dp,nums,i+1);
        dp[i] = max(include,exclude);
        return dp[i];
     }
+
+    int solveUsingTab(vector<int>& nums){
+        //step 1 create an dp array
+         int n = nums.size();
+        vector<int> dp(n+2,0);
+
+       //step2 analyse base cases and update array
+        dp[n]=0;
+
+       //checkparameter and reverse it run a loop
+
+       for(int i=n-1;i>=0;i--){
+          //step2:-If ans coming with recursion store ans in dp and return
+          int include = nums[i] + dp[i+2];
+          int exclude = 0 + dp[i+1];
+          dp[i] = max(include,exclude);
+       } 
+          return dp[0];
+         
+    }
    
     int rob(vector<int>& nums) {
     //return solverec(nums,0);
     //step 1:- Create dp array ans pass
-      int n = nums.size();
-      vector<int> dp(n+1,-1);
-      int i=0;
-      return SolveusingMem(dp,nums,i);
+    //   int n = nums.size();
+    //   vector<int> dp(n+1,-1);
+    //   int i=0;
+     // return SolveusingMem(dp,nums,i);
+       return solveUsingTab(nums);
+
     }
 };
