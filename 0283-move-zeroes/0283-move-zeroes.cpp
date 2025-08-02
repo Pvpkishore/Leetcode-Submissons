eddfbdea8b237349611bf0596955450af98bdfe0
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-08-02 10:24:54 +0530

    Time: 0 ms (100%), Space: 23.8 MB (53.8%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0; // Index to place the next non-zero element
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[j++], nums[i]); // Swap non-zero element to the front
-            }
-        }
+
+       int j=0;
+       for(int i=0;i<nums.size();i++){
+          if(nums[i]!=0){
+            swap(nums[i],nums[j]);
+            j++;
+          }
+       }
     }
 };
