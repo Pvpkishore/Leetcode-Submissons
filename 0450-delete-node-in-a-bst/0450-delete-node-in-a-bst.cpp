30ca981279864829f415bc2a3310ccd1bcf3bc7d
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-08-05 00:10:28 +0530

    Time: 0 ms (100%), Space: 34.4 MB (30%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -20,16 +20,18 @@ public:
 
         else if (key > root->val) {
             root->right = deleteNode(root->right, key);
-        } 
+        }
 
         else {
             // Node found
+            //if node has right child then we have have to delete right child only
             if (!root->left) {
                 TreeNode* temp = root->right;
                 delete root;
                 return temp;
             }
-            
+
+            //if node has left child then we have have to delete left child only
             else if (!root->right) {
                 TreeNode* temp = root->left;
                 delete root;
@@ -40,15 +42,22 @@ public:
             TreeNode* minNode = findMin(root->right);
             root->val = minNode->val;
             root->right = deleteNode(root->right, minNode->val);
+            
+            //Since you can’t just remove the node (it has children), 
+            // you need to replace it with another node in a way that maintains 
+            // the Binary Search Tree (BST) properties.
+            
         }
-
+        
         return root;
     }
 
     TreeNode* findMin(TreeNode* node) {
+
         while (node && node->left) {
             node = node->left;
         }
+
         return node;
     }
 };
