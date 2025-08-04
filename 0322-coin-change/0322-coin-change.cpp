1f3e46d1d05257e6f4365817ce27e027f8e76a89
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-08-04 23:22:56 +0530

    Time: 36 ms (55.27%), Space: 19.1 MB (61.81%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+///Using recursion TLE will be coming
+
+    int solve(vector<int>& coins, int amount) {
+
+        // basecase
+        if (amount == 0) {
+            return 0;
+        }
+
+        int mincoinsans = INT_MAX;
+
+        for (int i = 0; i < coins.size(); i++) {
+
+            int coin = coins[i];
+
+            if (coin <= amount) {
+                int recursionans = solve(coins, amount - coin);
+
+                if (recursionans != INT_MAX) {
+                    int coinsused = 1 + recursionans;
+                    mincoinsans = min(mincoinsans, coinsused);
+                }
+
+            }
+        }
+
+        return mincoinsans;
+    }
+
+//Using Memoisation method top to bottom
+     int solveMem(vector<int>& coins, int amount,vector<int> &dp) {
+
+        // basecase
+        if (amount == 0) {
+            return 0;
+        }
+
+        if(dp[amount]!=-1){
+            return dp[amount];
+        }
+
+        int mincoinsans = INT_MAX;
+
+        for (int i = 0; i < coins.size(); i++) {
+
+            int coin = coins[i];
+
+            if (coin <= amount) {
+                int recursionans = solveMem(coins, amount - coin,dp);
+
+                if (recursionans != INT_MAX) {
+                    int coinsused = 1 + recursionans;
+                    mincoinsans = min(mincoinsans, coinsused);
+                }
+
+            }
+        }
+
+        dp[amount] = mincoinsans;
+        return dp[amount];
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+
+        // int ans = solve(coins, amount);
+
+        //step:-1 create 1 d dp array
+        int n = coins.size();
+        vector<int> dp(amount+1,-1);
+        int ans = solveMem(coins, amount,dp);
+
+        if (ans == INT_MAX) {
+            return -1;
+        }
+
+        return ans;
+    }
+};
