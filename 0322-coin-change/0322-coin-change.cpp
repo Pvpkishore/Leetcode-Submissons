339d1354cad42f80ee917dabe47fd75de7b74b0c
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-08-04 23:45:12 +0530

    Time: 15 ms (93.54%), Space: 17.9 MB (75.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
 ///Using recursion TLE will be coming
-
     int solve(vector<int>& coins, int amount) {
 
         // basecase
@@ -29,7 +28,7 @@ public:
         return mincoinsans;
     }
 
-//Using Memoisation method top to bottom
+     //Using Memoisation method top to bottom
      int solveMem(vector<int>& coins, int amount,vector<int> &dp) {
 
         // basecase
@@ -62,14 +61,47 @@ public:
         return dp[amount];
     }
 
+    //using tabulation bootom to up
+    int solveUsingTab(vector<int>& coins, int amount){
+        // step1 :- craete 1 d dp array
+        vector<int> dp(amount+1,-1);
+
+        //step:-2 analyse base case
+        dp[0] =0;
+
+        for(int amt =1;amt<=amount;amt++){
+          //copy paste
+            int mincoinsans = INT_MAX;
+
+        for (int i = 0; i < coins.size(); i++) {
+
+            int coin = coins[i];
+
+            if (coin <= amt) {
+                int recursionans = dp[amt - coin];
+
+                if (recursionans != INT_MAX) {
+                    int coinsused = 1 + recursionans;
+                    mincoinsans = min(mincoinsans, coinsused);
+                }
+
+            }
+        }
+          dp[amt] = mincoinsans;
+        }
+
+        return dp[amount];
+    }
+
     int coinChange(vector<int>& coins, int amount) {
 
         // int ans = solve(coins, amount);
 
         //step:-1 create 1 d dp array
         int n = coins.size();
-        vector<int> dp(amount+1,-1);
-        int ans = solveMem(coins, amount,dp);
+        // vector<int> dp(amount+1,-1);
+        // int ans = solveMem(coins, amount,dp);
+        int ans = solveUsingTab(coins, amount);
 
         if (ans == INT_MAX) {
             return -1;
