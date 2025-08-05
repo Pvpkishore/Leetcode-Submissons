1fa51490f107a6c4fee6ce9b9e8eba6b6bf7fae3
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-08-05 14:38:01 +0530

    Time: 87 ms (9.59%), Space: 94.4 MB (12.6%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
+
         unordered_map<int,int> prefix;
         prefix[0]=1; // Base case: 0 odd numbers seen initially
         int oddcount =0,result =0;
+
 
         for(auto num:nums){
 
             if(num % 2!=0) oddcount++;
 
-           // If oddCount - k seen before, then we found valid subarrays
+            // If oddCount - k seen before, then we found valid subarrays
             if(prefix.find(oddcount-k)!=prefix.end()){
 
                 result+=prefix[oddcount-k];
 
             }
-            
+
             // Update the prefix map
             prefix[oddcount]++;
 
         }
+        
         return result;
+
     }
 };
