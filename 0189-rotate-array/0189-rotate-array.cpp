5d5d7e3ed8ff160c1d2b1200106c5f4f5bfaa7ff
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-09-16 11:04:48 +0530

    Time: 0 ms (100%), Space: 30.5 MB (10.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     void rotate(vector<int>& arr, int k) {
+
         vector<int> temp(arr.size());
+
         for (int i = 0; i < arr.size(); i++) {
+
             temp[(i + k) % arr.size()] = arr[i];
+            
         }
+
+     // Left rotate by 'd' positions
+    //  std::rotate(arr.begin(), arr.begin() + d, arr.end());
+
+
+    // Right rotate by 'd' positions
+    // std::rotate(arr.rbegin(), arr.rbegin() + d, arr.rend());
         arr = temp;
     }
 };
