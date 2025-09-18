8b4677178f0b311f2ddd1f36afe868d6c415d2da
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-09-18 11:21:17 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,18 +5,22 @@ public:
      int i = n-2;
       //first decreasing element from the end
        while(i>=0 && nums[i]>=nums[i+1]){ //stop when it decreases
-        i--;  
+          i--;  
        }
-       
+
        //find the element just larger than the nums[i] on the right
        int j=n-1;
+
        if(i>=0)
        {
-              while(nums[i]>=nums[j]){
-              j--;
+              while(nums[i]>=nums[j])
+              {
+                j--;
               }
               swap(nums[i],nums[j]);
+
        }
+
       reverse(nums.begin()+i+1,nums.end());
     }
 };
