23ccf9b874e907d9d9a5dd876af0be2af4dc53ae
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-09-20 15:24:25 +0530

    Time: 4 ms (22.15%), Space: 17.6 MB (83.06%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -4,8 +4,10 @@ public:
         // Traverse the string from the end to find the last odd digit
         for (int i = num.length() - 1; i >= 0; i--) {
             if ((num[i]) % 2 != 0) {
+
+                string largestoddstring =num.substr(0, i + 1);
                 // Return the substring from the beginning to the last odd digit
-                return num.substr(0, i + 1);
+                return largestoddstring;
             }
         }
         // If no odd digit is found, return an empty string
