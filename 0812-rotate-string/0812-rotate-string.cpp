c5e376dc16c929d19b40ca6481f52e966c71891e
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-09-20 15:25:19 +0530

    Time: 0 ms (100%), Space: 8 MB (84%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+
         // Step 1: Check if lengths match
         if (s.length() != goal.length()) return false;
 
         // Step 2: Concatenate s with itself
         string doubled = s + s;
-
+    
         // Step 3: Check if goal is a substring of doubled
         return doubled.find(goal) != string::npos;
+
     }
 };
