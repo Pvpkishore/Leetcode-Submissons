6b7f46ccb72f60c98c9d43fa5d4084e0b34dcd8f
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-09-20 15:56:51 +0530

    Time: 0 ms (100%), Space: 11.5 MB (31.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -8,16 +8,20 @@ public:
          freqMap[c]++;
       }  
 
-      vector<pair<char,int>> freqVec(freqMap.begin(), freqMap.end());
+    vector<pair<char,int>> freqVec(freqMap.begin(), freqMap.end());
 
-      sort(freqVec.begin(), freqVec.end(), [](pair<char, int> a, pair<char, int> b) {
+     sort(freqVec.begin(), freqVec.end(), 
+         [](pair<char, int> a, pair<char, int> b) {
             return a.second > b.second; // Sort in descending order of frequency
-        });
+         });
         
+
         string result = "";
+
         for (auto& p : freqVec) {
             result += string(p.second, p.first); // Append character p.first, p.second times
         }
+
 
         return result;
     }
